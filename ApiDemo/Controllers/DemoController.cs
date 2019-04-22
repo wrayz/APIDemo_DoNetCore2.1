5f2bfdc0c962720817137d3797bb6beb1632f2a8
Ray <ruizhouwu@gmail.com>
2019-04-23 01:20:51 +0800

    feat: GetDemo Api

---
+++ ApiDemo/Controllers/DemoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly DemoContext _context;
+
+        public DemoController(DemoContext context)
+        {
+            _context = context;
+
+            if (_context.Demo.Count() == 0)
+            {
+                // Create a new Demo if collection is empty,
+                // which means you can't delete all Demo.
+                _context.Demo.Add(new Demo { Name = "Demo1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Demo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Demo>>> GetDemo()
+        {
+            return await _context.Demo.ToListAsync();
+        }
+    }
+}
