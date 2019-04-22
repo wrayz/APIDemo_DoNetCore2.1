556ccbcbe58015a0c37a991ac55e9e605df25563
Ray <ruizhouwu@gmail.com>
2019-04-23 01:08:24 +0800

    ORM: EF Core Db First

---
+++ DataAccess/Models/DemoContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataAccess.Models
+{
+    public partial class DemoContext : DbContext
+    {
+        public DemoContext()
+        {
+        }
+
+        public DemoContext(DbContextOptions<DemoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Demo> Demo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=WIN-RAYZ\\MSSQLSERVER_2012;Database=Demo;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Demo>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("NAME")
+                    .HasMaxLength(10);
+            });
+        }
+    }
+}
